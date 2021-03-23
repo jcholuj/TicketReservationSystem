9a492497c44d6dc1dd5db474f43490bf7800d2c0
M3LNYK <48330869+M3LNYK@users.noreply.github.com>
2021-03-23 14:09:49 +0200

    Adding project initialization.

---
+++ src/main/java/com/awt_group1/ticketreservationsystem/TicketReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.awt_group1.ticketreservationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketReservationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketReservationSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/awt_group1/ticketreservationsystem/TicketReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awt_group1.ticketreservationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketReservationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
