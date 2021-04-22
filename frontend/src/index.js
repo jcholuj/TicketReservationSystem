11ed7174b7b4316fdca33914ab2db32d7a03f8a7
M3LNYK <48330869+M3LNYK@users.noreply.github.com>
2021-04-23 01:12:38 +0300

    Created login form Added SecSecurityConfig responsible for passwords in our app. Added html files for form page and successful login. Added js files for Form and Input responsible for login logic.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import Form from './Form'
+
+const inputs = [{
+    name: "username",
+    placeholder: "username",
+    type: "text"
+},{
+    name: "password",
+    placeholder: "password",
+    type: "password"
+},{
+    type: "submit",
+    value: "Submit",
+    className: "btn"
+}]
+
+const props = {
+    name: 'loginForm',
+    method: 'POST',
+    action: '/perform_login',
+    inputs: inputs
+}
+
+const params = new URLSearchParams(window.location.search)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <Form {...props} error={params.get('error')} />,
+    document.getElementById('container'))
